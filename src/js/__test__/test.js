e94104f7a7e4d6723f1e5a4d7403b88777493797
Artem18 <urart88@mail.ru>
2022-09-28 22:06:38 +0400

    DZ8-2--Containers доработка1

--- src/js/__test__/test.js
+++ src/js/__test__/test.js
@@ -1,25 +1,18 @@
 /* eslint-disable no-unused-vars */
-import { ErrorRepository, eks2 } from '../task2';
+import ErrorRepository from '../task2';
 
 test('TEST-OK1', () => {
   const f = () => {
-    const code = 3;
+    const eks2 = new ErrorRepository();
+    const code = '3';
     eks2.translate(code);
     return 'Ошибка #3';
   };
   expect(f()).toBe('Ошибка #3');
 });
 
-test('TEST-2(typeof)', () => {
-  const code = '3';
-  const f = () => {
-    eks2.translate(code);
-    return code;
-  };
-  expect(f()).toBe('3');
-});
-
-test('TEST-3-ERR', () => {
+test('TEST-2-ERR', () => {
+  const eks2 = new ErrorRepository();
   const code = 10;
   const f = () => {
     eks2.translate(code);
--- src/js/task2.js
+++ src/js/task2.js
@@ -1,17 +1,12 @@
-/* eslint-disable no-param-reassign */
-/* eslint-disable consistent-return */
-export class ErrorRepository {
+export default class ErrorRepository {
   constructor() {
     this.map = new Map([['1', 'Ошибка #1'], ['2', 'Ошибка #2'], ['3', 'Ошибка #3']]);
   }
 
   translate(code) {
-    this.map.forEach((item, idx) => {
-      if (idx === code) return item;
-    });
-    if (typeof (code) === 'number') code = String(code);
-    if (!this.map.has(code)) throw new Error('Ошибка! Такого кода нет.');
+    if (this.map.has(code)) {
+      return this.map.get(code);
+    }
+    throw new Error('Ошибка! Такого кода нет.');
   }
 }
-export const eks2 = new ErrorRepository();
-// eks2.translate(3);
